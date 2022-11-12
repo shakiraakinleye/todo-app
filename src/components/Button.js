1b0f0476ff7d26958660848c588d275a549d9611
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-11 17:53:49 -0700

    feat:  added a button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,8 @@
+import "./styles/button.css"
+
 export function Button({ buttonClass, buttonText, onClick }) {
   return (
-    <button className={buttonClass} onClick={onClick}>
+    <button className={"button " +  buttonClass} onClick={onClick}>
       {buttonText}
     </button>
   );
