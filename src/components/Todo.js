5bdd0f63757e126f47c21429536d83f8134fde1b
Shakira Akinleye <shakiraakinleye@gmail.com>
2022-11-17 00:29:30 +0100

    refactor: diplayed todo and buttons from variables

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,80 +10,83 @@ export function Todo({
   const [isEditing, setIsEditing] = useState(false);
   const dispatchTodo = useContext(TodoDispatchContext)
 
+  let todoDisplay = isEditing ? (
+    <div className="edit__todo">
+      <Input
+        value={todo.title}
+        onChange={(e) =>
+          dispatchTodo({
+            type: "edited",
+            id: todo.id,
+            todo: {
+              ...todo,
+              title: e.target.value,
+            }
+          })
+        }
+      />
+      <Input
+        value={todo.desc}
+        onChange={(e) =>
+          dispatchTodo({
+            type: "edited",
+            id: todo.id,
+            todo: {
+              ...todo,
+              desc: e.target.value
+            }
+          })
+        }
+      />
+    </div>
+  ) : (
+    <div className="todo">
+      <input
+        type="checkbox"
+        className="todo__checkbox"
+        defaultChecked={todo.done}
+        onChange={() => {
+          dispatchTodo({
+            type: "edited",
+            id: todo.id,
+            todo: {
+              ...todo,
+              done: !todo.done
+            }
+          })
+        }}
+      ></input>
+      <div className="todo__details">
+        <h1 className="todo__title">{todo.title}</h1>
+        <p className="todo__desc">{todo.desc}</p>
+      </div>
+    </div>
+  )
+
+  let buttonDisplayed =  isEditing ? (
+    <Button
+      buttonClass={"btn--save__todo"}
+      buttonText={"Save"}
+      onClick={() => {
+        setIsEditing(false);
+      }}
+    />
+  ) : (
+    <Button
+      buttonClass={"btn--edit__todo"}
+      buttonText={"Edit"}
+      onClick={() => {
+        setIsEditing(true);
+      }}
+    />
+  )
+
+
   return (
     <div className="todo__container">
-      {isEditing ? (
-        <div className="edit__todo">
-          <Input
-            value={todo.title}
-            onChange={(e) =>
-              dispatchTodo({
-                type: "edited",
-                id: todo.id,
-                todo: {
-                  ...todo,
-                  title: e.target.value,
-                }
-              })
-            }
-          />
-          <Input
-            value={todo.desc}
-            onChange={(e) =>
-              dispatchTodo({
-                type: "edited",
-                id: todo.id,
-                todo: {
-                  ...todo,
-                  desc: e.target.value
-                }
-              })
-            }
-          />
-        </div>
-      ) : (
-        <div className="todo">
-          <input
-            type="checkbox"
-            className="todo__checkbox"
-            defaultChecked={todo.done}
-            onChange={() => {
-              dispatchTodo({
-                type: "edited",
-                id: todo.id,
-                todo: {
-                  ...todo,
-                  done: !todo.done
-                }
-              })
-            }}
-          ></input>
-          <div className="todo__details">
-            <h1 className="todo__title">{todo.title}</h1>
-            <p className="todo__desc">{todo.desc}</p>
-          </div>
-        </div>
-      )}
-
+      {todoDisplay}
       <div className="btns__container">
-        {isEditing ? (
-          <Button
-            buttonClass={"btn--save__todo"}
-            buttonText={"Save"}
-            onClick={() => {
-              setIsEditing(false);
-            }}
-          />
-        ) : (
-          <Button
-            buttonClass={"btn--edit__todo"}
-            buttonText={"Edit"}
-            onClick={() => {
-              setIsEditing(true);
-            }}
-          />
-        )}
-
+        {buttonDisplayed}
         <Button
           buttonClass={"btn--delete__todo"}
           buttonText={"Delete"}
